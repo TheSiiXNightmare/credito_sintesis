227cdca1dc35bba7663f11b632fc40760b3c37a3
MLAlejandro <96034.joan23@fje.edu>
2016-05-04 02:50:21 +0200

    Buenas Noches
    Encontrado tutorial de ultima hora ^^

---
+++ Juego/scrips/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginController : MonoBehaviour {
+
+	private const string Login = "Last";
+	private const string Pass = "1234";
+	[SerializeField]
+	private InputField usuarioField = null;
+	[SerializeField]
+	private InputField paswordField = null;
+	[SerializeField]
+	private Text Feedbaking = null;
+	[SerializeField]
+	private Toggle RememberData = null;
+
+	
+	// Use this for initialization
+	void Start () {
+		if (PlayerPrefs.HasKey ("memoria") && PlayerPrefs.GetInt ("memoria") == 1) {
+			usuarioField.text = PlayerPrefs.GetString ("rememberUsuario");
+			paswordField.text = PlayerPrefs.GetString ("rememberContrasena");
+		}
+	}
+	
+	// Update is called once per frame
+	public void logIn () {
+		string usuario = usuarioField.text;
+		string contrasena = paswordField.text;
+
+		if (RememberData.isOn) {
+			PlayerPrefs.SetInt ("memoria", 1);
+			PlayerPrefs.SetString ("rememberUsuario", usuario);
+			PlayerPrefs.SetString ("rememberContrasena", contrasena);
+		}
+
+		if (usuario == Login && contrasena == Pass) {
+			Feedbaking.CrossFadeAlpha (100f, 0f, false);
+			Feedbaking.color = Color.green;
+			Feedbaking.text = "Conectado correctamente\nCargando juego...";
+			StartCoroutine (CargarNivel ());
+		} else {
+			Feedbaking.CrossFadeAlpha (100f, 1f, false);
+			Feedbaking.color = Color.red;
+			Feedbaking.text = "Usuario o contrasena incorrectos";
+			Feedbaking.CrossFadeAlpha (0f, 2f, false);
+			usuarioField.text = "";
+			paswordField.text = "";
+		}
+	}
+
+	IEnumerator CargarNivel() {
+		yield return new WaitForSeconds (5);
+		SceneManager.LoadScene ("nivel");
+	}
+}
---
+++ Juego/scrips/colector.js
@@ -0,0 +1,50 @@
+﻿
+var coins;
+coins = 10;
+var continua : boolean = true;
+var timer : float = 0.0;
+var plusTime : float = 0.0;
+var guiShow : boolean = false;
+
+// funcion que nos mostrara el tiempo y las monedas que nos faltan por cojer
+function OnGUI()
+{
+	GUI.Box(Rect(10, 20, 35, 20), "" + timer.ToString("0"));
+	
+	if(guiShow == true)
+	{
+		GUI.Box(Rect(65, 20, 35, 20), "+" + plusTime.ToString("0"));
+		ShowGUI();
+	}
+	if (continua) {
+		GUILayout.Label( "Monedas restantes: " + coins );
+	}else{
+		GUILayout.Label( "felicidades has acabado en: "  + coins + " segundos" );
+	}
+}
+
+function ShowGUI()
+{
+	yield WaitForSeconds(2);
+	guiShow = false;
+}
+
+//funcion que hace que las monedas desaparezcan en el momento en el que nuestro personaje entra en contacto con ellas y una vez llegado a 0 cambia el valor de continua a false
+function OnTriggerEnter(other: Collider) {
+	if (other.gameObject.tag == "coin") {
+		Destroy(other.gameObject);
+		coins--;
+		if (coins == 0) {
+			continua = false;
+			coins = timer;
+			
+		}
+	}
+}
+// funcion que actualiza mediante el update, el tiempo que ha transcurrido desde que empieza a jugar hasta que continua cambie su valor a false
+function Update()
+{
+	if (continua) {
+		timer += Time.deltaTime;
+	}
+}
