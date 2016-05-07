873b78322a82bdc0a8201624e5980461612ad9a4
AGAPITO <agitoalex@gmail.com>
2016-05-07 15:44:15 +0200

    control video intro

---
+++ Juego/scrips/PlayMovieOnSpace.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class PlayMovieOnSpace : MonoBehaviour {
+	MovieTexture movie;
+	Renderer r;
+
+	void Start() {
+		r = GetComponent<Renderer>();
+		movie = (MovieTexture)r.material.mainTexture;
+		StartCoroutine(CargarNivel ());
+	}
+
+	IEnumerator CargarNivel() {
+		movie.Play();
+		print ("antes de segundos");
+		yield return new WaitForSeconds (movie.duration);
+		print ("despues de segundos");
+		SceneManager.LoadScene ("finciona");
+	}
+
+}
